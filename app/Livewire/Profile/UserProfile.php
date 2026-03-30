0118a5d9bd26018df6b49b969dbb2f7e84ffbe40
Akmal <akmalthemalong@gmail.com>
2026-03-30 08:04:02 +0800

    feat: tidak semua field bisa dirubah

--- app/Livewire/Profile/UserProfile.php
+++ app/Livewire/Profile/UserProfile.php
@@ -17,21 +17,27 @@ class UserProfile extends Component
     public $desaUser;
     public $kecamatanUser;
 
-    public function mount() {
+    public function mount()
+    {
         $user = getActiveUser();
-        $user->load('desa.kecamatan');
 
-        $this->desaUser = $user->desa ? $user->desa->nama : '-';
-        $this->kecamatanUser = $user->desa->kecamatan ? $user->desa->kecamatan->nama : '-';
+        if (method_exists($user, 'desa')) {
+            $user->load('desa.kecamatan');
+            $this->desaUser = $user->desa ? $user->desa->nama : '-';
+            $this->kecamatanUser = $user->desa?->kecamatan ? $user->desa->kecamatan->nama : '-';
+        } else {
+            $this->desaUser = '-';
+            $this->kecamatanUser = '-';
+        }
 
-        $this->form->fillFromModel($user);
+        $this->form->fillFromModel($user, getActiveGuard());
     }
 
     public function save()
     {
-            $this->form->update();
+        $this->form->update();
 
-            $this->notifySuccess('Berhasil menyimpan perubahan profile');
+        $this->notifySuccess('Berhasil menyimpan perubahan profile');
 
     }
 
